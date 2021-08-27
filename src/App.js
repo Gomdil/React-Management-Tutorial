b230bd805d79a0d19b45d828226bfe2c7c43033e
LeeYoungmyun <dark1288@naver.com>
2021-08-27 10:09:04 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import './App.css';
 import { Custmer , CustmerProfile } from './components/Custmer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { makeStyles } from '@material-ui/core/styles'
+
+
+
+const useStyle = makeStyles({
+  root : {
+    width:"100%",
+    marginTop : 20,
+    overflowX : "auto"
+  },
+  table:{
+    minWidth : 1080
+  }
+})
 
 
 const custmer = [
@@ -29,27 +49,40 @@ const custmer = [
 }
 ]
 
-function App() {
+export default function App() {    
+  const  classes = useStyle();
   return (
-    <div>
-      {
-        custmer.map(c => {
-          return (
-          <Custmer
-          key = {c.id}
-          id = {c.id}
-          image = {c.image}      
-          name = {c.name}
-          birthday = {c.birthday}
-          gender = {c.gender}
-          job = {c.job}
-        /> 
-        )   
-        })
-      }      
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            custmer.map(c => {
+              return (
+              <Custmer
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}      
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            /> 
+            )   
+            })
+          }  
+        </TableBody>
+      </Table>    
       
-    </div>
+    </Paper>
   );
 }
-
-export default App;
--- src/components/Custmer.js
+++ src/components/Custmer.js
@@ -1,37 +1,15 @@
 import React from 'react'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 export const Custmer = (props) => {
     return (
-        <div>
-           <CustmerProfile
-                id={props.id}
-                image={props.image}
-                name={props.name}                
-           />
-           <CustmerInfo
-                birthday={props.birthday}
-                gender={props.gender}
-                job={props.job}
-           />
-        </div>
-    )
-}
-
-export const CustmerProfile = (props) =>{
-    return (
-        <div>
-            <img src={props.image} alt="profile"/>
-            <h2>{props.name}({props.id})</h2>
-        </div>
-    )
-}
-
-
-export const CustmerInfo = (props) =>{
-    return(
-       <div>
-           <p>{props.birthday}</p>
-           <p>{props.gender}</p>
-           <p>{props.job}</p>
-       </div>
+        <TableRow>
+            <TableCell>{props.id}</TableCell>
+            <TableCell><img src={props.image} alt='profile'/></TableCell>
+            <TableCell>{props.name}</TableCell>
+            <TableCell>{props.birthday}</TableCell>
+            <TableCell>{props.gender}</TableCell>
+            <TableCell>{props.job}</TableCell>
+        </TableRow>
     )
 }
